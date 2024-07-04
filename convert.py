fae77d11e45f0244395e2b4ecced09c2169c9a1f
KnowScratcher <yianlee2008@gmail.com>
2024-07-04 08:13:24 +0800

    增加更多解釋

--- convert.py
+++ convert.py
@@ -3,4 +3,4 @@ import os
 
 with zipfile.ZipFile("test.cjz","w") as z:
     for i in os.listdir("./structure"):
-        z.write(f"./structure/{i}",compress_type=zipfile.ZIP_STORED)
+        z.write(f"./structure/{i}",compress_type=zipfile.ZIP_STORED,arcname=f"{i}")
---
+++ read.py
@@ -0,0 +1,4 @@
+import zipfile
+
+zip = zipfile.ZipFile("./test.cjz","r")
+print(zip.namelist())
