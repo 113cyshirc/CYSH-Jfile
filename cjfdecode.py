cdbe7a073d301dcfef7c2fcfa97094b8e0d75906
KnowScratcher <yianlee2008@gmail.com>
2024-07-07 09:42:09 +0800

    update file format check

--- cjfdecode.py
+++ cjfdecode.py
@@ -2,10 +2,16 @@
 # Quick Note
 - This decoder doesn't support cjf v1
 - all value will be stored as str for v2
-
+## v2 types
+    - dict  [name]
+    - list  [l:name]
+    - str   [s:name]
+    - int   [d:name]
+    - float [f:name]
 """
 
 from io import TextIOWrapper
+import json
 
 class FileFormatError(Exception):
     def __init__(self, msg:str="") -> None:
@@ -24,55 +30,60 @@ def loadscjf(text:str) -> dict:
     type_now = "str"
     result = {}
     lines = text.split("\n")
-    for i in lines:
+    file_format = 0
+    for i in lines: # read each line
         if len(result) == 0 and i.startswith("CYSHJ file format v"): # get the format
             result["fileformat"] = int(i[19:])
+            file_format = result["fileformat"]
             if result["fileformat"] == 1:
                 print("[warning] Decoder doesn't support file format v1, and it might produce some error.")
-        elif i == "" or i.startswith("//"):
+        elif i == "" or i.startswith("//"): # ignore comments and blank line
             continue
         else:
-            if i.startswith("[") and i.endswith("]"):
-                class_tag = i[1:-1]
-                type_analyze = class_tag.split(":")
-                if len(type_analyze) == 1:
-                    type_now = "dict"
-                    result[class_tag] = {} # create a new dict
-                else:
-                    match type_analyze[0]:
-                        case "l":
-                            type_now = "list"
-                            result[type_analyze[1]] = []
-                            class_tag = type_analyze[1]
-                        case "s":
-                            type_now = "str"
-                            result[type_analyze[1]] = ""
-                            class_tag = type_analyze[1]
-                        case "d":
-                            type_now = "int"
-                            result[type_analyze[1]] = 0
-                            class_tag = type_analyze[1]
-                        case "f":
-                            type_now = "float"
-                            result[type_analyze[1]] = 0.0
-                            class_tag = type_analyze[1]
-                        case _:
-                            raise FileFormatError("Unsupported Type")
-            else:
-                match type_now:
-                    case "dict":
-                        
-                        key,value = i.split(":")
-                        result[class_tag][key] = value
-                    case "list":
-                        result[class_tag].append(i)
-                    case "str":
-                        result[class_tag] = i
-                    case "int":
-                        result[class_tag] = int(i)
-                    case "float":
-                        result[class_tag] = float(i)
-
+            match file_format:
+                case 2:
+                    if i.startswith("[") and i.endswith("]"): # read tag
+                        class_tag = i[1:-1]
+                        type_analyze = class_tag.split(":") # normal tag
+                        if len(type_analyze) == 1: # type is a dict
+                            type_now = "dict"
+                            result[class_tag] = {} # create a new dict
+                        else:
+                            match type_analyze[0]: # other types
+                                case "l":
+                                    type_now = "list"
+                                    result[type_analyze[1]] = []
+                                    class_tag = type_analyze[1]
+                                case "s":
+                                    type_now = "str"
+                                    result[type_analyze[1]] = ""
+                                    class_tag = type_analyze[1]
+                                case "d":
+                                    type_now = "int"
+                                    result[type_analyze[1]] = 0
+                                    class_tag = type_analyze[1]
+                                case "f":
+                                    type_now = "float"
+                                    result[type_analyze[1]] = 0.0
+                                    class_tag = type_analyze[1]
+                                case _:
+                                    raise FileFormatError("Unsupported Type")
+                    else:
+                        match type_now:
+                            case "dict":
+                                
+                                key,value = i.split(":")
+                                result[class_tag][key] = value
+                            case "list":
+                                result[class_tag].append(i)
+                            case "str":
+                                result[class_tag] = i
+                            case "int":
+                                result[class_tag] = int(i)
+                            case "float":
+                                result[class_tag] = float(i)
+                case _:
+                    raise FileFormatError("Unsupported Format Version.")
     return result
 
 
