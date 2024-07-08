b5a217f477ddeab06b3ef45a903fe31fc4fe5c6c
KnowScratcher <yianlee2008@gmail.com>
2024-07-08 15:21:53 +0800

    cjt子題支援準備

--- decode.py
+++ decode.py
@@ -1,7 +1,8 @@
 import cjfdecode
 
-with open("./structure/question.cjf","r",encoding="utf-8") as f:
-    print(cjfdecode.loadcjf(f))
+# with open("./structure/question.cjf","r",encoding="utf-8") as f:
+#     print(cjfdecode.loadcjf(f))
 
-    # for i in cjfdecode.loadcjt(f):
-    #     print(i.input,i.output)
+with open("./structure/tests.cjt","r",encoding="utf-8") as f:
+    for i in cjfdecode.loadcjt(f):
+        print(i.input,i.output)
